c2c0a0158d0fc935302dbe75413d6694794b873f
juan reyna <juanreyna@hotmail.com.ar>
2022-05-16 22:16:19 -0300

    entrega 2

---
+++ ecomerce/src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>BebeShops</h1>
+            <div>
+                <button>Ropa</button>
+                <button>Accesorios</button>
+                <button>Juguetes</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
